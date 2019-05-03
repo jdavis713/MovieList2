dc405408e6160c45b34853c1300bc8560f938eb9
Jordan Davis <jordan.m.davis92@gmail.com>
2019-05-03 11:33:05 -0400

    Initial commit

---
+++ MovieList/MovieList/MovieListTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  MovieListTableViewCell.swift
+//  MovieList
+//
+//  Created by Jordan Davis on 5/3/19.
+//  Copyright © 2019 Jordan Davis. All rights reserved.
+//
+
+import UIKit
+
+class MovieListTableViewCell: UITableViewCell {
+
+    //MARK: -Actions
+    
+    
+    
+    
+    //MARK: -Functions
+    private func updateViews() {
+        guard let movie = movie else { return }
+        
+        self.movieTitleLabel.text = movie.title
+    }
+    
+    
+    //MARK: -Properties
+   @IBOutlet var movieTitleLabel: UILabel!
+    
+        var movie: Movie? {
+        didSet {
+            updateViews()
+        }
+    }
+
+}
---
+++ MovieList/MovieList/MovieListTableViewController.swift
@@ -0,0 +1,92 @@
+//
+//  MovieListTableViewController.swift
+//  MovieList
+//
+//  Created by Jordan Davis on 5/3/19.
+//  Copyright © 2019 Jordan Davis. All rights reserved.
+//
+
+import UIKit
+
+protocol MovieEnteredDelegate: class {
+    func movieWasEntered(movie: Movie)
+}
+class MovieListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieListController.movies.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieListCell", for: indexPath)
+
+        let movie = movieListController.movies[indexPath.row]
+        cell.textLabel?.text = movie.title
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "AddMovieToList" {
+            guard let addNewMovieVC = segue.destination as? AddNewMovieViewController,
+                let indexPath = tableView.indexPathForSelectedRow else { return }
+            addNewMovieVC.movie = movieListController.movies[indexPath.row]
+        }
+    }
+    
+    
+    //MARK: -Properties
+    var movieListController = MovieListController()
+    
+}
